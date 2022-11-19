c246376f5d43d92340056873467496b0ab4f031b
Halil Kaya <halilkaya@teknasyon.com>
2022-11-19 19:51:13 +0300

    test(gateway): Add unit test to user.controller.

--- apps/gateway/src/modules/user/user.controller.spec.ts
+++ apps/gateway/src/modules/user/user.controller.spec.ts
@@ -1,6 +1,10 @@
 import {UserController} from "./user.controller";
 import {Test, TestingModule} from "@nestjs/testing";
 import {ClientProxy, ClientsModule, Transport} from "@nestjs/microservices";
+import {Types} from "mongoose";
+import {firstValueFrom, of, throwError} from "rxjs";
+import {UserCreateDto} from "./dto";
+import {NicknameAlreadyTakenException} from "@app/exceptions";
 
 describe('UserController', () => {
     let mockUserController: UserController
@@ -25,9 +29,49 @@ describe('UserController', () => {
         mockUserServiceClient = module.get<ClientProxy>('USER_SERVICE')
     })
 
-    it('should be defined', function () {
+    it('should be defined', () => {
         expect(mockUserController).toBeDefined()
         expect(mockUserServiceClient).toBeDefined()
     });
 
+    describe('create', () => {
+
+        it('should create user', async () => {
+            const mockResult = {
+                _id: new Types.ObjectId().toString(),
+                fullName: 'halil-kaya',
+                nickname: 'hlk',
+                password: 'password',
+                createdAt: new Date()
+            }
+            const mock = await jest.spyOn(mockUserServiceClient, 'send')
+            mock.mockImplementation(() => of(mockResult))
+            const reqDto: UserCreateDto = {
+                fullName: mockResult.fullName,
+                nickname: mockResult.nickname,
+                password: mockResult.password
+            }
+            const user = await firstValueFrom(mockUserController.create(reqDto))
+            expect(user._id).toBeDefined()
+            expect(user.fullName).toBe(reqDto.fullName)
+            expect(user.nickname).toBe(reqDto.nickname)
+            expect(user.password).toBe(reqDto.password)
+            expect(user.createdAt).toBeDefined()
+        });
+
+        it('should throw nickname is already taken error', async () => {
+            const mock = await jest.spyOn(mockUserServiceClient, 'send')
+            mock.mockImplementation(() => throwError(new NicknameAlreadyTakenException()))
+            const reqDto: UserCreateDto = {
+                fullName: 'halil-kaya',
+                nickname: 'hlk',
+                password: 'p@ssword'
+            }
+            try {
+                await firstValueFrom(mockUserController.create(reqDto))
+            } catch (err) {
+                expect(err).toBeInstanceOf(NicknameAlreadyTakenException)
+            }
+        })
+    })
 })
