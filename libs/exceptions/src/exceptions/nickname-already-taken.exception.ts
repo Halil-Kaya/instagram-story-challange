7c21f531e14c1cf574117fb8fc18c4ae2250b198
Halil Kaya <halilkaya@teknasyon.com>
2022-11-19 19:21:17 +0300

    fix(exception): Update Nickname Already Taken error code.

--- libs/exceptions/src/exceptions/nickname-already-taken.exception.ts
+++ libs/exceptions/src/exceptions/nickname-already-taken.exception.ts
@@ -3,6 +3,6 @@ import {ErrorCodes} from "@app/exceptions/error-codes";
 
 export class NicknameAlreadyTakenException extends CustomException {
     constructor() {
-        super('Nickname is already taken', 500, ErrorCodes.USERNAME_ALREADY_TAKEN);
+        super('Nickname is already taken', 500, ErrorCodes.NICKNAME_ALREADY_TAKEN);
     }
 }
