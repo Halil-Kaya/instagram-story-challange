a00c52b2d5c3b832a4703c12bb084a51d29f3b50
Halil Kaya <halilkaya@teknasyon.com>
2022-11-19 19:23:37 +0300

    test(user): Add user create tests.

--- apps/gateway/e2e/api/user/create.e2e-spec.ts
+++ apps/gateway/e2e/api/user/create.e2e-spec.ts
@@ -1,5 +1,8 @@
 import {UserCreateDto} from "../../../src/modules/user/dto";
 import {createUser} from "../../common/user.helper";
+import {IUser} from "@app/interfaces/user.interface";
+import {MetaInterface} from "@app/interceptors";
+import {ErrorCodes} from "@app/exceptions/error-codes";
 
 it('should create user', async () => {
     const reqDto: UserCreateDto = {
@@ -7,9 +10,26 @@ it('should create user', async () => {
         nickname: Math.random().toString(36).slice(2, 16),
         password: 'passw@rd'
     }
-    const {data: user} = await createUser(reqDto)
+    const {data} = await createUser(reqDto)
+    const user = <IUser>data.result
     expect(user._id).toBeDefined()
     expect(user.fullName).toBe(reqDto.fullName)
     expect(user.nickname).toBe(reqDto.nickname)
     expect(user.createdAt).toBeDefined()
 });
+
+it('should throw nickname is already taken error', async () => {
+    const reqDto: UserCreateDto = {
+        fullName: '#test-user',
+        nickname: Math.random().toString(36).slice(2, 16),
+        password: 'passw@rd'
+    }
+    await createUser(reqDto)
+
+    try {
+        await createUser(reqDto)
+    } catch (err) {
+        const result = <MetaInterface>err.response.data.meta;
+        expect(result.errorCode).toBe(ErrorCodes.NICKNAME_ALREADY_TAKEN);
+    }
+})
--- apps/gateway/e2e/common/user.helper.ts
+++ apps/gateway/e2e/common/user.helper.ts
@@ -2,9 +2,10 @@ import {UserCreateDto} from "../../src/modules/user/dto";
 import {IUser} from "@app/interfaces/user.interface";
 import {baseUri} from "./index";
 import axios, {AxiosResponse} from 'axios';
+import {Response} from "@app/interceptors/transform.interceptor";
 
 const uri = baseUri + "user/"
 
-export const createUser = (dto: UserCreateDto): Promise<AxiosResponse<IUser>> => {
+export const createUser = (dto: UserCreateDto): Promise<AxiosResponse<Response<IUser>>> => {
     return axios.post(uri + 'create', dto)
 }
