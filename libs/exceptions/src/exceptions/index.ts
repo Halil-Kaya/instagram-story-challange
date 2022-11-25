c790ad6ad5b1b4836a132905543ca4e580d33bf9
Halil Kaya <halilkaya@teknasyon.com>
2022-11-25 22:00:17 +0300

    feat(exception-lib): Add invalid-credentials.exception.

--- libs/exceptions/src/exceptions/index.ts
+++ libs/exceptions/src/exceptions/index.ts
@@ -1,1 +1,2 @@
 export * from './nickname-already-taken.exception'
+export * from './invalid-credentials.exception'
---
+++ libs/exceptions/src/exceptions/invalid-credentials.exception.ts
@@ -0,0 +1,8 @@
+import {CustomException} from "@app/exceptions/exceptions/custom.exception";
+import {ErrorCodes} from "@app/exceptions/error-codes";
+
+export class InvalidCredentialsException extends CustomException {
+    constructor() {
+        super('Invalid Credentials', 500, ErrorCodes.INVALID_CREDENTIALS);
+    }
+}
