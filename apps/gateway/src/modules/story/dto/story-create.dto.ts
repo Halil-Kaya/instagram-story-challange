c77990ec770c06fe8e94d5a03c1e55a3007c28f6
Halil Kaya <halilkaya@teknasyon.com>
2022-12-01 00:27:15 +0300

    feat(gateway): Add StoryCreate dto.

---
+++ apps/gateway/src/modules/story/dto/story-create.dto.ts
@@ -0,0 +1,13 @@
+import {IsString, MaxLength, MinLength} from "class-validator";
+
+export class StoryCreateDto {
+    @MinLength(1)
+    @MaxLength(24)
+    @IsString()
+    title: string;
+
+    @MinLength(1)
+    @MaxLength(24)
+    @IsString()
+    content: string;
+}
