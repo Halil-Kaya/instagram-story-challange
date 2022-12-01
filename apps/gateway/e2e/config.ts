ca369d087c79b1a848f9cdfc7b2cce03e45de75b
Halil Kaya <halilkaya@teknasyon.com>
2022-12-01 23:11:46 +0300

    test(db): Add postgresql database helper.

--- apps/gateway/e2e/config.ts
+++ apps/gateway/e2e/config.ts
@@ -1,5 +1,12 @@
 export const config = {
     mongoConnectionUrl: 'mongodb://localhost:27016/isc',
     redisConnectionUrl: 'redis://localhost:6378',
-    redisUserExpireTime: 2592000
+    redisUserExpireTime: 2592000,
+    postgresql: {
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'isc'
+    }
 }
