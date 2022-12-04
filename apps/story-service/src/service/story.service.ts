ca587b6ecc5afdea8af6a9edf4940e0e8674518f
Halil Kaya <halilkaya@teknasyon.com>
2022-12-04 13:07:58 +0300

    feat(story-service): Emit to queueu-service in story create flowchart.

--- apps/story-service/src/service/story.service.ts
+++ apps/story-service/src/service/story.service.ts
@@ -1,14 +1,18 @@
-import {Injectable} from "@nestjs/common";
+import {Inject, Injectable} from "@nestjs/common";
 import {StoryRepository} from "../repository/story.repository";
-import {StoryServicePayloads} from '@app/payloads'
+import {QueueServicePatterns, Services, StoryServicePayloads, QueueServicePayloads} from '@app/payloads'
 import {IStory} from "@app/interfaces";
+import {ClientProxy} from "@nestjs/microservices";
 
 @Injectable()
 export class StoryService {
-    constructor(private readonly storyRepository: StoryRepository) {
+    constructor(private readonly storyRepository: StoryRepository,
+                @Inject(Services.QUEUE_SERVICE) private queueServiceClient: ClientProxy) {
     }
 
     async create(payload: StoryServicePayloads.Create): Promise<IStory> {
-        return this.storyRepository.create(payload)
+        const createdStory = await this.storyRepository.create(payload)
+        this.queueServiceClient.emit<void, QueueServicePayloads.AddToQueue>(QueueServicePatterns.ADD_TO_QUEUE, {id: createdStory.id})
+        return createdStory
     }
 }
--- apps/story-service/src/story.module.ts
+++ apps/story-service/src/story.module.ts
@@ -4,9 +4,21 @@ import {Story} from "./model/story.model";
 import {StoryController} from "./controller/story.controller";
 import {StoryService} from "./service/story.service";
 import {StoryRepository} from "./repository/story.repository";
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {Services} from "@app/payloads";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Story])],
+    imports: [
+        TypeOrmModule.forFeature([Story]),
+        ClientsModule.register([
+            {
+                name: Services.QUEUE_SERVICE,
+                transport: Transport.TCP,
+                options: {
+                    host: 'queue-service'
+                }
+            },
+        ])],
     controllers: [StoryController],
     providers: [StoryService, StoryRepository],
 })
