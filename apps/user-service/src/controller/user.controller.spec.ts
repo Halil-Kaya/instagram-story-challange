dc687ddd09e80c4f8f27ea3b3348783941983a99
Halil Kaya <halilkaya@teknasyon.com>
2022-12-04 13:30:56 +0300

    style(prettier): Apply prettier.

--- apps/user-service/src/controller/user.controller.spec.ts
+++ apps/user-service/src/controller/user.controller.spec.ts
@@ -1,23 +1,23 @@
-import {UserController} from "./user.controller";
-import {UserService} from "../service/user.service";
-import {Test, TestingModule} from "@nestjs/testing";
-import {UserRepository} from "../repository/user.repository";
-import {IUser} from "@app/interfaces/user.interface";
-import {Types} from "mongoose";
-import {getModelToken} from "@nestjs/mongoose";
-import {User} from "../model/user.model";
-import {NicknameAlreadyTakenException} from "@app/exceptions";
+import { UserController } from './user.controller';
+import { UserService } from '../service/user.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserRepository } from '../repository/user.repository';
+import { IUser } from '@app/interfaces/user.interface';
+import { Types } from 'mongoose';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from '../model/user.model';
+import { NicknameAlreadyTakenException } from '@app/exceptions';
 
 describe('UserController', () => {
-    let mockUserController: UserController
-    let mockUserService: UserService
+    let mockUserController: UserController;
+    let mockUserService: UserService;
     const mockUserModel: IUser = {
         _id: new Types.ObjectId().toString(),
         fullName: 'halil-kaya',
         nickname: 'hlk',
         password: 'password',
-        createdAt: new Date()
-    }
+        createdAt: new Date(),
+    };
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -27,17 +27,17 @@ describe('UserController', () => {
                 UserController,
                 {
                     provide: getModelToken(User.name),
-                    useValue: mockUserModel
-                }
-            ]
-        }).compile()
-        mockUserController = module.get<UserController>(UserController)
-        mockUserService = module.get<UserService>(UserService)
-    })
+                    useValue: mockUserModel,
+                },
+            ],
+        }).compile();
+        mockUserController = module.get<UserController>(UserController);
+        mockUserService = module.get<UserService>(UserService);
+    });
 
     it('should defined', () => {
-        expect(mockUserController).toBeDefined()
-        expect(mockUserService).toBeDefined()
+        expect(mockUserController).toBeDefined();
+        expect(mockUserService).toBeDefined();
     });
 
     describe('create', () => {
@@ -47,34 +47,34 @@ describe('UserController', () => {
                 fullName: 'halil-kaya',
                 nickname: 'hlk',
                 password: 'password',
-                createdAt: new Date()
-            }
-            const mock = await jest.spyOn(mockUserService, 'create')
-            mock.mockImplementation(() => Promise.resolve(mockResult))
+                createdAt: new Date(),
+            };
+            const mock = await jest.spyOn(mockUserService, 'create');
+            mock.mockImplementation(() => Promise.resolve(mockResult));
             const reqDto = {
                 fullName: 'halil-kaya',
                 nickname: 'hlk',
                 password: 'password',
-            }
-            expect(await mockUserController.create(reqDto)).toBe(mockResult)
-        })
+            };
+            expect(await mockUserController.create(reqDto)).toBe(mockResult);
+        });
 
         it('should throw nickname is already taken error', async () => {
-            const mock = await jest.spyOn(mockUserService, 'create')
+            const mock = await jest.spyOn(mockUserService, 'create');
             mock.mockImplementation(() => {
-                throw new NicknameAlreadyTakenException()
-            })
+                throw new NicknameAlreadyTakenException();
+            });
             const reqDto = {
                 fullName: 'halil-kaya',
                 nickname: 'hlk',
                 password: 'password',
-            }
+            };
             try {
-                await mockUserController.create(reqDto)
+                await mockUserController.create(reqDto);
             } catch (err) {
-                expect(err).toBeInstanceOf(NicknameAlreadyTakenException)
+                expect(err).toBeInstanceOf(NicknameAlreadyTakenException);
             }
-        })
+        });
     });
 
     describe('getUserForLogin', () => {
@@ -84,13 +84,13 @@ describe('UserController', () => {
                 fullName: 'halil-kaya',
                 nickname: 'hlk',
                 password: 'password',
-                createdAt: new Date()
-            }
-            const mock = await jest.spyOn(mockUserService, 'getUserForLogin')
-            mock.mockImplementation(() => Promise.resolve(mockResult))
+                createdAt: new Date(),
+            };
+            const mock = await jest.spyOn(mockUserService, 'getUserForLogin');
+            mock.mockImplementation(() => Promise.resolve(mockResult));
 
-            const result = await mockUserController.getUserForLogin({nickname: mockResult._id})
-            expect(result.password).toBeDefined()
+            const result = await mockUserController.getUserForLogin({ nickname: mockResult._id });
+            expect(result.password).toBeDefined();
         });
-    })
-})
+    });
+});
