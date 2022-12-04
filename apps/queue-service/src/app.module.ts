8da6ce99c39a7c6aaf472a66033f0be39b0ace41
Halil Kaya <halilkaya@teknasyon.com>
2022-12-04 13:12:59 +0300

    feat(queue): Add Bull for jobs.

--- apps/queue-service/src/app.module.ts
+++ apps/queue-service/src/app.module.ts
@@ -1,5 +1,8 @@
 import {Module} from "@nestjs/common";
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {QueueModule} from "./queue.module";
+import {BullModule} from "@nestjs/bull";
+import {IEnvironment} from "./environment.interface";
 
 @Module({
     imports: [
@@ -7,6 +10,17 @@ import {ConfigModule} from "@nestjs/config";
             envFilePath: 'environments/queue-service.env',
             isGlobal: true
         }),
+        BullModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService<IEnvironment>) => ({
+                redis: {
+                    host: configService.get<string>('REDIS_HOST'),
+                    port: configService.get<number>('REDIS_PORT'),
+                },
+            }),
+            inject: [ConfigService]
+        }),
+        QueueModule
     ],
     controllers: [],
     providers: [],
--- apps/queue-service/src/queue.module.ts
+++ apps/queue-service/src/queue.module.ts
@@ -1,9 +1,18 @@
 import {Module} from "@nestjs/common";
+import {QueueController} from "./controller/queue.controller";
+import {QueueService} from "./service/queue.service";
+import {BullModule} from "@nestjs/bull";
+import {StoryConsumer} from "./consumer/story.consumer";
+import {JobQueues} from "./jobs";
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: []
+    imports: [
+        BullModule.registerQueue({
+            name: JobQueues.STORY,
+        })
+    ],
+    controllers: [QueueController],
+    providers: [QueueService, StoryConsumer]
 })
 export class QueueModule {
 
