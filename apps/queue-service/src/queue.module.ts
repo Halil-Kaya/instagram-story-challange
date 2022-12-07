cdd435d9c62cc4756b469bc75196bbc08a2f1806
Halil Kaya <halilkaya@teknasyon.com>
2022-12-08 01:08:59 +0300

    feat(queue-service): Add delete endpoint.

--- apps/queue-service/src/queue.module.ts
+++ apps/queue-service/src/queue.module.ts
@@ -1,17 +1,29 @@
-import { Module } from '@nestjs/common';
-import { QueueController } from './controller/queue.controller';
-import { QueueService } from './service/queue.service';
-import { BullModule } from '@nestjs/bull';
-import { StoryConsumer } from './consumer/story.consumer';
-import { JobQueues } from './jobs';
+import {Module} from '@nestjs/common';
+import {QueueController} from './controller/queue.controller';
+import {QueueService} from './service/queue.service';
+import {BullModule} from '@nestjs/bull';
+import {StoryConsumer} from './consumer/story.consumer';
+import {JobQueues} from './jobs';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {Services} from "@app/payloads";
 
 @Module({
     imports: [
         BullModule.registerQueue({
             name: JobQueues.STORY,
         }),
+        ClientsModule.register([
+            {
+                name: Services.STORY_SERVICE,
+                transport: Transport.TCP,
+                options: {
+                    host: 'story-service',
+                },
+            },
+        ]),
     ],
     controllers: [QueueController],
     providers: [QueueService, StoryConsumer],
 })
-export class QueueModule {}
+export class QueueModule {
+}
--- apps/queue-service/src/service/queue.service.ts
+++ apps/queue-service/src/service/queue.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { QueueServicePayloads } from '@app/payloads';
-import { Queue } from 'bull';
-import { InjectQueue } from '@nestjs/bull';
-import { ConfigService } from '@nestjs/config';
-import { IEnvironment } from '../environment.interface';
-import { DeleteStoryJob, JobNames, JobQueues } from '../jobs';
+import {Injectable} from '@nestjs/common';
+import {QueueServicePayloads} from '@app/payloads';
+import {Queue} from 'bull';
+import {InjectQueue} from '@nestjs/bull';
+import {ConfigService} from '@nestjs/config';
+import {IEnvironment} from '../environment.interface';
+import {DeleteStoryJob, JobNames, JobQueues} from '../jobs';
 
 @Injectable()
 export class QueueService {
     constructor(
         @InjectQueue(JobQueues.STORY) private storyQueue: Queue,
         private readonly configService: ConfigService<IEnvironment>,
-    ) {}
+    ) {
+    }
 
     async addStoryToQueue(payload: QueueServicePayloads.AddToQueue): Promise<void> {
         await this.storyQueue.add(JobNames.DELETE_STORY, <DeleteStoryJob>payload, {
